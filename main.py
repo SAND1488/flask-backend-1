5c4f228e32912f169faa70d91d6154406a75ae95
muhammadnozim <nozimbert@gmail.com>
2025-10-22 20:36:54 +0500

    Update 2

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ app = Flask("__name__")
 
 @app.route("/")
 def home():
-    return "Salom"
+    return render_template("index.html")
+
+@app.route("/contact")
+def contacts():
+    return render_template("contact.html")
+
+@app.route("/about")
+def abouts():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
